5300be94ac2be9c6aa1868ed866d6fafb983dcea
cowsskategood <cowsskategood@gmail.com>
2022-03-11 17:10:28 -0500

    First Commit

---
+++ Module copy/Commands.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+
+
+namespace CowBot.Module
+{
+
+
+
+
+
+
+
+
+
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("moo")]
+        public async Task Moo()
+        {
+            await ReplyAsync("Moo");
+        }
+
+        [Command("time")]
+        public async Task time()
+        {
+            DateTime date1 = DateTime.Now;
+
+            DateTime date2 = DateTime.UtcNow;
+
+            await ReplyAsync($"Your time: {date1:HH:mm:ss} and {date2:HH:mm:ss} in torn");
+
+        }
+
+
+       
+
+    }
+}
